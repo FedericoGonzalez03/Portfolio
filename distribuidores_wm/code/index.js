7350cb464ed20cb7e9ef0878ce488baf143badba
FeFinho <03.federico.gonzalez@gmail.com>
2022-08-04 00:48:36 -0300

    Update index.js

--- distribuidores_wm/code/index.js
+++ distribuidores_wm/code/index.js
@@ -47,9 +47,8 @@ function listarSinPrecio() {
     let container = document.getElementById('gridCont');
     container.innerHTML = '<p id="sinRes" class="visually-hidden" style="grid-column: 1 / main-end;display:block;text-align:center;font-size:30px;vertical-align:middle;">No se encontraron productos para su busqueda...</p>'
     fetch('./apis/listar.php')
-        .then(datas => {
-            container.innerHTML = `${datas}`
-            alert(JSON.stringify(datas))
+        .then(res => {
+            container.innerHTML = res
         })
 }
 
