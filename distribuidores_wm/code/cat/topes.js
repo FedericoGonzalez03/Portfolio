9081234ad9851670623009d1e3de56fe1b5512b4
FeFinho <03.federico.gonzalez@gmail.com>
2022-08-05 21:04:55 -0300

    Update topes.js

--- distribuidores_wm/code/cat/topes.js
+++ distribuidores_wm/code/cat/topes.js
@@ -2,7 +2,7 @@ window.onload = inicio;
 
 function inicio() {
     console.log(window.location)
-    if(window.location.href == './topes.html'){
+    if(window.location.href == 'https://distribuidoreswm.federicogs.com/routes/categorias/topes.html'){
         window.location.href = window.location.href + "?w1=" + 'Topes y Aislacion';
     }
     verificarSesion();
