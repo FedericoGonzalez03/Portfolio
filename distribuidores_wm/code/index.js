ddb04d4064db24a9ea41b95ab75e6e22e12e4df5
FeFinho <03.federico.gonzalez@gmail.com>
2022-08-09 11:43:19 -0300

    Update index.js

--- distribuidores_wm/code/index.js
+++ distribuidores_wm/code/index.js
@@ -49,9 +49,10 @@ function listarSinPrecio() {
     fetch('./apis/listarSinPrecio.php')
         .then(res => res.json())
         .then(datas => {
-            for(i in datas){
-                container.innerHTML += `<div id="page${i}">${datas[i]}</div>`
-            }
+            // for(i in datas){
+            //     container.innerHTML += `<div id="page${i}">${datas[i]}</div>`
+            // }
+            container.innerHTML += datas[0]
         })
 }
 
