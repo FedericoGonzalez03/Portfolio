d21879641ca2e99c283267d6184c0f3b586abaf4
FeFinho <03.federico.gonzalez@gmail.com>
2022-08-09 12:32:51 -0300

    Update index.js

--- distribuidores_wm/code/index.js
+++ distribuidores_wm/code/index.js
@@ -60,9 +60,9 @@ function listarSinPrecio() {
                 }
             }
             pages.innerHTML = `${actualPage+1} de ${datas.length}`
+            container.innerHTML +=
+                '<p id="sinRes" class="visually-hidden" style="grid-column: 1 / main-end;display:block;text-align:center;font-size:30px;vertical-align:middle;">No se encontraron productos para su busqueda...</p>';
         });
-        container.innerHTML =
-            '<p id="sinRes" class="visually-hidden" style="grid-column: 1 / main-end;display:block;text-align:center;font-size:30px;vertical-align:middle;">No se encontraron productos para su busqueda...</p>';
     }
 
 function verificarSesion() {
