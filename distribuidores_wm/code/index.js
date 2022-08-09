dc8463005efaf9feac5aca9aa2143a060435fefa
FeFinho <03.federico.gonzalez@gmail.com>
2022-08-09 13:08:50 -0300

    Update index.js

--- distribuidores_wm/code/index.js
+++ distribuidores_wm/code/index.js
@@ -80,9 +80,9 @@ function listarSinPrecio() {
             productos.innerHTML = "";
             for (i in datas) {
                 if (i == actualPage) {
-                    productos.innerHTML += `<h5>Página ${actualPage+1}</h5><div style="margin-bottom:15px;" id="page${i}" class="gridCont actualPage">${datas[i]}</div>`;
+                    productos.innerHTML += `<h5>Página ${parseInt(i)+1}</h5><div style="margin-bottom:15px;" id="page${i}" class="gridCont actualPage">${datas[i]}</div>`;
                 } else {
-                    productos.innerHTML += `<h5>Página ${actualPage+1}</h5><div style="margin-bottom:15px;" id="page${i}" class="gridCont visually-hidden">${datas[i]}</div>`;
+                    productos.innerHTML += `<h5>Página ${parseInt(i)+1}</h5><div style="margin-bottom:15px;" id="page${i}" class="gridCont visually-hidden">${datas[i]}</div>`;
                 }
             }
             pages.innerHTML = `${actualPage + 1} de ${datas.length}`;
