29fa804f663c15bd8f3df643d4547d47b7c44e7e
Federico González <03.federico.gonzalez@gmail.com>
2022-10-08 02:21:33 -0300

    a

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function slideLeft(){
+  document.getElementById('projectsSet').scrollBy(-0.162 * window.innerWidth,0)
+    if(document.getElementById('projectsSet').scrollLeft/window.innerWidth < 0.1){
+    document.getElementById('projectsSet').scrollBy(-1000,0)
+  }
+}
 
+function slideRight(){
+  document.getElementById('projectsSet').scrollBy(0.163 * window.innerWidth,0)
+  if(document.getElementById('projectsSet').scrollLeft/window.innerWidth >0.6){
+    document.getElementById('projectsSet').scrollBy(1000,0)
+  }
+  let startWidth = getComputedStyle(document.getElementById('projectsSet')).getPropertyValue('--my-start-width');
+  console.log(startWidth)
+}
