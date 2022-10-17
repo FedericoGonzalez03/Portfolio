880cc16d2fa27c3539b628a38f98eee2a97ca24b
Federico González <03.federico.gonzalez@gmail.com>
2022-10-17 00:35:14 -0300

    splashscreen

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const splashscreen = document.getElementById('splash');
+
+
 function slideLeft(){
   document.getElementById('projectsSet').scrollBy(-0.162 * window.innerWidth,0);
   if(document.getElementById('projectsSet').scrollLeft/window.innerWidth < 0.1){
@@ -11,3 +14,9 @@ function slideRight(){
     document.getElementById('projectsSet').scrollBy(1000,0)
   }
 }
+
+
+window.addEventListener("DOMContentLoaded", ()=>{
+  setTimeout(()=>splashscreen.classList.add('active'),1500)
+  
+})
