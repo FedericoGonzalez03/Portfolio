7f5460fd826f7b416c0ecbd01dcc5cb95008a873
Federico González <03.federico.gonzalez@gmail.com>
2022-10-17 01:20:18 -0300

    aaaa

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const splashscreen = document.getElementById('splash');
-
+const form  = document.getElementById('formContact');
 
 function slideLeft(){
   document.getElementById('projectsSet').scrollBy(-0.162 * window.innerWidth,0);
@@ -19,4 +19,16 @@ function slideRight(){
 window.addEventListener("DOMContentLoaded", ()=>{
   setTimeout(()=>splashscreen.classList.add('active'),1500)
   
+})
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  let formulario = new FormData(form);
+
+  let send = {
+      from_name: document.querySelector('#name').value,
+      from_email: document.querySelector('#email').value,
+      message: document.querySelector('#message').value
+  }
+  emailjs.send('service_p5cw83c', 'template_k5tppxg', send);
 })
