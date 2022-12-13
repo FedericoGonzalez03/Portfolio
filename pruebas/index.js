423dc4693e83d9cd45866eeeaf9aa906f5099daa
Federico González <03.federico.gonzalez@gmail.com>
2022-12-13 01:08:50 -0300

    Update index.js

--- pruebas/index.js
+++ pruebas/index.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     const btn = document.querySelector('button');
     btn.addEventListener('click', async () => {
-        const res = await fetch('pruebas.federicogs.com/api')
+        const res = await fetch('/api')
         const data = await res.json();
         console.log(data);
     });
