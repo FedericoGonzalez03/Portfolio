568198aeae9e57ff62d7e5c7f999071ad3afebd7
Federico González <03.federico.gonzalez@gmail.com>
2023-03-20 00:12:59 -0300

    Update App.jsx

--- adminCollectingDev/src/App.jsx
+++ adminCollectingDev/src/App.jsx
@@ -46,7 +46,7 @@ function App() {
               tel: tel,
               mail: mail}
 
-            fetch('http://www.myrestapis.space/collecting/add-business', {
+            fetch('http:aa//www.myrestapis.space/collecting/add-business', {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json'
