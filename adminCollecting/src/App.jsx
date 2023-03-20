3fd5f342686b78a7d1443820d7056e40afba15f1
Federico González <03.federico.gonzalez@gmail.com>
2023-03-19 23:35:01 -0300

    asda

--- adminCollecting/src/App.jsx
+++ adminCollecting/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import { TextField, Autocomplete, Button } from "@mui/material";
+import { TextField, Autocomplete, Button, Divider } from "@mui/material";
 
 const empresas = [];
 
@@ -10,33 +10,18 @@ function App() {
   return (
     <div className="App">
       <section>
-        <h2>Alta de empresa</h2>
+        <h2>Alta de Cliente</h2>
         <form action="">
           <TextField
+            id="razonBusiness"
             className="inp"
             color="error"
             label="Razon Social"
             variant="standard"
           />
-          <Autocomplete
-            className="inp"
-            options={empresas}
-            renderInput={(params) => (
-              <TextField
-                color="error"
-                variant="standard"
-                {...params}
-                label="Es cliente de"
-              />
-            )}
-          />
+
           <TextField
-            className="inp"
-            color="error"
-            label="Direccion"
-            variant="standard"
-          />
-          <TextField
+            id="telBusiness"
             className="inp"
             color="error"
             label="Telefono"
@@ -44,42 +29,42 @@ function App() {
           />
 
           <TextField
+            id="mailBusiness"
             className="inp"
             color="error"
-            label="Fecha (día / día1-día2 / día-fin)"
+            label="Mail"
             variant="standard"
           />
 
-          <TextField
-            className="inp"
-            color="error"
-            label="Horario (hora1 - hora2)"
-            variant="standard"
-          />
+          <Button color="info" className="btn" variant="contained"
+          onClick={()=>{
+            const razon = document.querySelector('#razonBusiness').value;
+            const tel = document.querySelector('#telBusiness').value;
+            const mail = document.querySelector('#mailBusiness').value;
+            const data = 
+              {razon: razon,
+              tel: tel,
+              mail: mail}
 
-          <TextField
-            className="inp"
-            color="error"
-            label="Demora (minutos)"
-            variant="standard"
-          />
-          <TextField
-            className="inp"
-            color="error"
-            label="Latitud"
-            variant="standard"
-          />
-          <TextField
-            className="inp"
-            color="error"
-            label="Longitud"
-            variant="standard"
-          />
-          <Button color="info" className="btn" variant="contained">
+            fetch('https://www.myrestapis.space/collecting/add-bussiness', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(data)
+            }).then(response => {
+              console.log('response', response);
+            })
+            .catch(error => {
+              console.log('error', error);
+            });
+
+          }}>
             Agregar
           </Button>
         </form>
       </section>
+      <Divider></Divider>
       <section>
         <h2>Alta de empresa</h2>
         <form action="">
